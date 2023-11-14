0721b3bd0b36e6ad20d219feccac5babf9f59a08
ChrisEssomba <chris.essomba357@gmail.com>
2023-11-14 09:48:19 +0100

    navBar-comments

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { NavBar } from "./components/NavBar";
 //import { Banner } from "./components/Banner";
 
+//<h1 className="headerContainer">Portofolio</h1>
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <p>chris</p>
+      
  
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+//I import all the librairies needed : react, images, haslink 
 import React, { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
@@ -6,15 +7,20 @@ import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 
+//I import the router who's useful to handle the dom
 import {
   BrowserRouter as Router
 } from "react-router-dom";
 
+
+//I create the object navbar that will be export into my app.js  
 export const NavBar = () => {
 
+    //first I create method to handle the links and the scrolling of the screen
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
 
+    //this method sets up the navbar according the scrolling level
   useEffect(() => {
     const onScroll = () => {
       if (window.scrollY > 50) {
@@ -29,6 +35,8 @@ export const NavBar = () => {
     return () => window.removeEventListener("scroll", onScroll);
   }, [])
 
+
+  // optdate the the state of the link
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
   }
@@ -38,7 +46,7 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="loga" />
+            <img src={logo} alt="logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
