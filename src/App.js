bc06cc7df80e7998f0e33b84445e5b6ccdc06d28
ChrisEssomba <chris.essomba357@gmail.com>
2023-11-14 22:49:31 +0100

    component-projets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+/* eslint-disable react/prop-types */
+
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Competences } from './components/Competences';
+import { Projects } from './components/Projets';
+import PropTypes from 'prop-types';
 
 //<h1 className="headerContainer">Portofolio</h1>
 function App() {
@@ -12,6 +16,7 @@ function App() {
       <NavBar />
       <Banner />
       <Competences />
+      <Projects />
       
  
     </div>
