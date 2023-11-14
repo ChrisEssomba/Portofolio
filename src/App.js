a3f29be76f08cd97658229e879bb4141959ce030
ChrisEssomba <chris.essomba357@gmail.com>
2023-11-14 10:10:38 +0100

    Banner creation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { NavBar } from "./components/NavBar";
-//import { Banner } from "./components/Banner";
+import { Banner } from "./components/Banner";
 
 //<h1 className="headerContainer">Portofolio</h1>
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
       
  
     </div>
